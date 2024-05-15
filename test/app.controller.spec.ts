b0a94589ecd700ae71a94bbf480dc5189e27c32b
Cliva Wolf <cliva.wolf@mail.ru>
2024-05-15 05:59:16 +0300

    clean

--- test/app.controller.spec.ts
+++ test/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '../src/app/rest-api-adapter/controller/app.controller';
+import { AppService } from '../src/domain/app.service';
 
 describe('AppController', () => {
   let appController: AppController;
